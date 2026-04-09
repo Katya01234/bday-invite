3f413b10c65e36d030c0d18c0cc91445faaf65ed
Kate Nikolaeva <3149134@gmail.com>
2026-04-09 22:22:36 +0300

    debag

--- src/constants/guests.js
+++ src/constants/guests.js
@@ -3,28 +3,28 @@ export const GUESTS = [
     id: 1,
     searchName: "артем", // Пишем маленькими для легкого поиска
     displayName: "Артемка",
-    geo: "Нижний Новгород",
+    geo: "Нижний",
     aliases: ["артем", "артемий", "artemiy", "artem"]
   },
   {
     id: 2,
     searchName: "даша",
     displayName: "Дашенька",
-    geo: "Нижний Новгород",
+    geo: "Нижний",
     aliases: ["даша", "дарья", "dasha"]
   },
   {
     id: 3,
     searchName: "оля",
     displayName: "Олечка",
-    geo: "Нижний Новгород",
+    geo: "Нижний",
     aliases: ["оля", "ольга", "olya"]
   },
   {
     id: 4,
     searchName: "вова",
     displayName: "Вова",
-    geo: "Нижний Новгород",
+    geo: "Нижний",
     aliases: ["вова", "владимир", "vladimir"]
   },
   {
